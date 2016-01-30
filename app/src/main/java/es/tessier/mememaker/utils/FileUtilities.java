f9b405174d1895616a9fd32c772087c7c6c8064f
Josecamacho22 <jcamachofernandez22@gmail.com>
2016-01-30 14:03:13 +0100

    Leer de Tarjeta Interna

--- app/src/main/java/es/tessier/mememaker/utils/FileUtilities.java
+++ app/src/main/java/es/tessier/mememaker/utils/FileUtilities.java
@@ -1,24 +1,58 @@
 package es.tessier.mememaker.utils;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Evan Anger on 7/28/14.
  */
 public class FileUtilities {
 
+    private static final String TAG = FileUtilities.class.getName();
+
     public static void saveAssetImage(Context context, String assetName) {
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory,assetName);
+        AssetManager assetManager = context.getAssets();
 
+        InputStream in = null;
+        FileOutputStream out = null;
 
+        try {
+            in = assetManager.open(assetName);
+            out = context.openFileOutput(
+                    fileToWrite.getAbsolutePath(),
+                    context.MODE_PRIVATE);
+            out = new FileOutputStream(fileToWrite);
 
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "FileNotFoundException caught ", e);
+        } catch (IOException e) {
+            Log.e(TAG,"IOException caught ",e);
+        } finally {
+
+            if (in != null)
+                try {
+                    in.close();
+                } catch (IOException e) {
+                }
+            if (out != null)
+                try {
+                    out.close();
+                } catch (IOException e) {
+                }
+
+        }
     }
 
     public static Uri saveImageForSharing(Context context, Bitmap bitmap,  String assetName) {
